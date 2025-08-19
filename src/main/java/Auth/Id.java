4b7fad9af68ada538cdedc71b188b26dc25f3cc5
Amrit478 <Amritraj4786@gmail.com>
2025-08-19 11:39:14 -0400

    new files

---
+++ src/main/java/Auth/Id.java
@@ -0,0 +1,19 @@
+package Auth;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+/*This class is used after user is successful login and than this class is accept the
+* Id and than see what the user is like admin, teacher or student and allow him the
+* permission based on that*/
+@Entity
+@Table(name = "ID")
+public class Id {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+
+}
